f0b102f2a773c8727e4ab284437d66ce01152c17
sebadehaedo <seba.deh97@gmail.com>
2020-09-04 15:48:22 -0300

    netflix clone

---
+++ build/precache-manifest.fd2339ace39033d9d46892b4d8fb4ae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39e30849adb4368a619f670a48d7b71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00a4c3a5405a425e1c50",
+    "url": "/static/css/main.e5a17394.chunk.css"
+  },
+  {
+    "revision": "2a2df4c95671f453aa1e",
+    "url": "/static/js/2.f7689358.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.f7689358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a4c3a5405a425e1c50",
+    "url": "/static/js/main.7261a12b.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
